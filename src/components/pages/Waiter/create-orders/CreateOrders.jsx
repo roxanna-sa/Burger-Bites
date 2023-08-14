0758537d5c81901c2ac6031bff742eb6fcbb79b6
Roxana <roxanna.sa@gmail.com>
2023-08-14 16:37:54 -0400

    Create order view

--- src/components/pages/Waiter/create-orders/CreateOrders.jsx
+++ src/components/pages/Waiter/create-orders/CreateOrders.jsx
@@ -1,7 +1,47 @@
+import Input from '../../../element/Input/Input.jsx';
+import CreateOrdersActions from './createOrdersActions.jsx';
+
 function CreateOrders(){
+
+  const {
+    getWaiterName
+  } = CreateOrdersActions();
+
   return (
     <>
-      <p>Creamos órdenes para los clientitos :D</p>
+      {/* Primera fila: mesero, nombre cliente, mesa y logo corporativo */}
+      <div className="row">
+
+        {/* Nombre mesero e inputs */}
+        <div className="col-6 ps-4 pe-2">
+          <label className="fw-bold mt-4">Waiter: {getWaiterName()}</label>
+
+          <Input inputId={'clientsName'} inputText={'Client\'s name'} />
+
+          <Input inputId={'tableNumber'} inputText={'Table nº'} inputType='number' />
+
+        </div>
+
+        {/* logo corporativo */}
+        <div className="col-6 pe-2">
+          <img src='/proyectImages/logoBurger.png' className='mt-3 mx-auto d-block' style={{height: '180px', width: 'auto'}}/>
+        </div>
+
+      </div>
+
+      {/* Segunda fila, lista de órdenes y resumen de productos */}
+
+      <div className='row'>
+      {/* Lista de ordenes  */}
+        <div className='col-6'>
+          <p>Creamos órdenes para los clientitos :D Creamos órdenes para los clientitos :D Creamos órdenes para los clientitos :D Creamos órdenes para los clientitos :D</p>
+        </div>
+
+        {/* Resumen de la orden */}
+        <div className='col-6'>
+          <p>Creamos órdenes para los clientitos :D Creamos órdenes para los clientitos :D Creamos órdenes para los clientitos :D Creamos órdenes para los clientitos :D</p>
+        </div>
+      </div>      
     </>
   );
 }
