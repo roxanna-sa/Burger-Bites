00c652f81d8cd11b7d7f63067cafb4e0b4ea9684
Roxana <roxanna.sa@gmail.com>
2023-08-17 22:47:45 -0400

    Replace nav Alerts with SA

--- src/components/pages/Login/login.jsx
+++ src/components/pages/Login/login.jsx
@@ -1,7 +1,7 @@
 import './login.scss';
 import LoginActions from './loginActions.jsx';
 import Input from '../../element/Input/Input';
-import { Exception } from 'sass';
+
 
 const Login = () => {
 
@@ -24,17 +24,8 @@ const Login = () => {
         </div>
       
         <form method="post" onSubmit={handleSubmit} className='col-6 offset-3'>
-          {/* <div className='form-group mt-3'>
-            <label htmlFor='email' className='form-label w-100 mb-0'>E-mail</label>
-            <input id='email' name='email' placeholder='E-mail' className='w-100 form-control'></input>
-          </div> */}
+    
           <Input inputId={'email'} inputText={'E-mail'} inputType={'text'} placeHolderText={'E-mail'} divClass='form-group mt-3' />
-
-
-          {/* <div className='form-group mt-2'>
-            <label htmlFor='password' className='form-label w-100 mb-0'>Password</label>
-            <input id='password' name='password' placeholder='Password' className='w-100 form-control'></input>
-          </div> */}
 
           <Input inputId={'password'} inputText={'Password'} inputType={'password'} placeHolderText={'Password'} divClass='form-group mt-3' />
 
