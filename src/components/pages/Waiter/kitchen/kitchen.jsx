8abd862796354f6553a4a3d000296a9bd5cab348
Roxana <roxanna.sa@gmail.com>
2023-08-30 21:00:55 -0400

    Create: display order

--- src/components/pages/Waiter/kitchen/kitchen.jsx
+++ src/components/pages/Waiter/kitchen/kitchen.jsx
@@ -1,9 +1,43 @@
+import KitchenActions from "./kitchenActions";
+import Utilities from "../../../utils/utilities";
+import OrderDisplay from "../../../element/orderDisplay/orderDisplay";
+
+
+
 function KitchenWaiter(){
+
+  const { orders } = KitchenActions();
+
+  const { getNameOfUserInSession } = Utilities();
+
   return (
-    <>
-      <p>Veo las ordenes en espera :D</p>
-    </>
+    <div className='container'>
+      <div className='row'>
+          <div className="col-6 ps-4 pe-2"> 
+            <label className="fw-bold mt-4">Waiter: {getNameOfUserInSession()}</label>
+          </div>
+
+          <div className="col-6 pe-2">
+            <img src='/proyectImages/logoBurger.png' className='mt-3 mx-auto d-block' style={{height: '180px', width: 'auto'}}/>
+          </div>
+      </div>
+
+      <div className='row'>
+        <p>Order Status</p>
+      </div>
+
+      <div className='row'>
+        { orders && orders.map((order, index) => {
+          return (
+            <div key={index} className='col-6'>
+              <OrderDisplay order={order} />
+            </div>
+          );
+        })}
+      </div>
+
+    </div>
   );
-}
+};
 
 export default KitchenWaiter;
