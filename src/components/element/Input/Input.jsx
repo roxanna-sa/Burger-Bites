3ebc7efe010b06f3ba6e917bae9b945c525357a7
Roxana <roxanna.sa@gmail.com>
2023-08-30 20:59:11 -0400

    Add parameter

--- src/components/element/Input/Input.jsx
+++ src/components/element/Input/Input.jsx
@@ -1,9 +1,9 @@
-const Input = ({ inputType = 'text', inputId, inputText, placeHolderText, divClass = 'form-group', setState = null }) => {
+const Input = ({ inputType = 'text', inputId, inputText, value, placeHolderText, divClass = 'form-group', setState = null }) => {
   return (
     <>
       <div className={divClass}>
         <label htmlFor={inputId} className='form-label w-100 mb-0'>{inputText}</label>
-        <input id={inputId} name={inputId} placeholder={placeHolderText} type={inputType} className='w-100 form-control' onKeyUp={setState}></input>
+        <input id={inputId} name={inputId} value={value} placeholder={placeHolderText} type={inputType} className='w-100 form-control' onChange={setState}></input>
       </div>
     </>
   );
