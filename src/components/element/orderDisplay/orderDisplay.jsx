ea38f1dd70cbc719f7b927012cd8eacf4cfd3579
Roxana <roxanna.sa@gmail.com>
2023-09-04 23:57:09 -0300

    Make it reusable: orderDisplay

--- src/components/element/orderDisplay/orderDisplay.jsx
+++ src/components/element/orderDisplay/orderDisplay.jsx
@@ -1,6 +1,7 @@
+import Action from '../buttons/actionButton/actionButton';
 import './orderDisplay.scss';
 
-const OrderDisplay = ({order}) => {
+const OrderDisplay = ({order, isCook}) => {
 
   const totalPrice = () => {
     let total = 0;
@@ -13,7 +14,7 @@ const OrderDisplay = ({order}) => {
   };
 
   return (
-      <div className="orderDisplayContainer mt-4">
+      <section className="orderDisplayContainer mt-4">
         <div className="row">
             <h4>Table n°: {order.table}</h4>
         </div>
@@ -26,7 +27,7 @@ const OrderDisplay = ({order}) => {
         </div>
 
         <div className='w-100 orderContainer'>
-          {order.products && order.products.map((product, index) => {
+          {order.products && order.products.map((product, index) => { {/* && verifica order.products existe y solo así realiza el mapeo*/}
               return (
                 <div key={index} className="row mx-0 px-0">
                   <div className="col-8">{product.name}</div>
@@ -40,9 +41,16 @@ const OrderDisplay = ({order}) => {
       
 
         <div className="row">
-          <h5>Total: ${totalPrice()}</h5>
+          <div className='col-8'>
+            <h5>Total: ${totalPrice()}</h5>
+          </div>
+
+          {isCook && <div className='col-4'>
+            <Action className='btn btn-letucce w-100' text='Ready' />
+          </div>}
         </div>
-      </div>
+        
+      </section>
   )
 }
 
